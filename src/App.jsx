0232fec825e7fd0eb02d3a4ea51f35d359590af1
devyan18 <epache17@gmail.com>
2023-02-16 05:46:10 -0300

    fix overflow error and add input value with command

--- src/App.jsx
+++ src/App.jsx
@@ -29,30 +29,32 @@ function App() {
       <div className='waifu_creator'>
         <form onSubmit={handleAddPejota} className="form">
           <h2>Agregar waifus</h2>
-          <span className='helper'>Usa el comando $mmi, copia todas los nombres de las waifus separandolas por un salto de linea "Enter"</span>
-          <textarea name="pejotas" cols="20" rows="20" />
+          <span className='helper'>Usa el comando $mm, copia todas los nombres de las waifus separandolas por un salto de linea "Enter"</span>
+          <textarea name="pejotas" cols="20" rows="30" />
           <button className='btn'>Ordenar Waifus</button>
         </form>
         <div className='order'>
           <h2>Verifica que las waifus se hayan ordenado corréctamente.</h2>
           <ul className='list'>
-            {waifus?.map((waifu, index) => (
+            {waifus.length > 0 ? waifus?.map((waifu, index) => (
               <li key={index}>{waifu}</li>
-            ))}
+            )) : <span>No hay waifus...</span>}
           </ul>
 
           <button
             className='btn'
             onClick={handleGenerateCommand}
           >Generar comando</button>
-          {
-            command && (
-              <button
-                className='btn'
-                onClick={() => copyTextToClipboard(command)}
-              >Copiar comando</button>
-            )
-          }
+          <div className='command'>
+            <input type="text" value={command} placeholder={'No existe comando.'} disabled={!command}/>
+            <button
+              className={`btn ${!command ? 'disabled' : ''}`}
+              onClick={() => copyTextToClipboard(command)}
+              disabled={!command}
+            >{
+                !command ? 'No existe comando.' : 'Copiar comando'
+              }</button>
+          </div>
 
         </div>
       </div>
